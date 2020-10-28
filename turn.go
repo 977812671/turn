b4c2c71f56e8f3846dfc395b28c5f8672ff65325
977812671 <977812671@qq.com>
2020-10-28 04:44:54 +0000

    turn

---
+++ turn.go
@@ -0,0 +1,34 @@
+// Package turn implements RFC 5766 Traversal Using Relays around NAT.
+package turn
+
+import (
+	"encoding/binary"
+
+	"github.com/977812671/stun"
+)
+
+// bin is shorthand for binary.BigEndian.
+var bin = binary.BigEndian
+
+// Default ports for TURN from RFC 5766 Section 4.
+const (
+	// DefaultPort for TURN is same as STUN.
+	DefaultPort = stun.DefaultPort
+	// DefaultTLSPort is for TURN over TLS and is same as STUN.
+	DefaultTLSPort = stun.DefaultTLSPort
+)
+
+var (
+	// AllocateRequest is shorthand for allocation request message type.
+	AllocateRequest = stun.NewType(stun.MethodAllocate, stun.ClassRequest)
+	// CreatePermissionRequest is shorthand for create permission request type.
+	CreatePermissionRequest = stun.NewType(stun.MethodCreatePermission, stun.ClassRequest)
+	// SendIndication is shorthand for send indication message type.
+	SendIndication = stun.NewType(stun.MethodSend, stun.ClassIndication)
+	// RefreshRequest is shorthand for refresh request message type.
+	RefreshRequest = stun.NewType(stun.MethodRefresh, stun.ClassRequest)
+	// ConnectRequest is shorthand for connect request message type.
+	ConnectRequest = stun.NewType(stun.MethodConnect, stun.ClassRequest)
+	// ConnectionBindRequest is shorthand for connect request message type.
+	ConnectBindRequest = stun.NewType(stun.MethodConnectionBind, stun.ClassRequest)
+)
